6638236685a3e2fb093ce726311dc5536bd09fab
Sojib Islam <sojibislam6505@gmail.com>
2025-12-17 18:53:41 +0600

    Forget Password

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,12 +1,13 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
 import { Link } from "react-router";
 import { auth } from "../../firebase.init";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { IoIosEye ,IoIosEyeOff} from "react-icons/io";
 
 export default function Login() {
   const [error, setError] = useState('')
   const [showPass, setShowPass] = useState(false)
+  const emailRef = useRef()
   const handleSignIn = e => {
       e.preventDefault()
       const email = e.target.email.value 
@@ -27,17 +28,30 @@ export default function Login() {
        setError('Something Wrong')
       })
   }
+  const handleForgetPass = () => {
+   const email = emailRef.current.value
+   sendPasswordResetEmail(auth, email)
+   .then(()=>{
+      alert('Send Password Reset Email ! Cheack your email')
+   }).catch((error) => {
+    setError(error.message)
+   })
+   console.log(email)
+  }
   return (
     <div className="flex justify-center mt-20">
       <form onSubmit={handleSignIn} className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
         <label className="label">Email</label>
-        <input type="email" name="email" className="input" placeholder="Email" />
+        <input type="email" name="email" ref={emailRef} className="input" placeholder="Email" />
         <label className="label">Password</label>
        <div className="relative">
          <input type={showPass?'text':'password'} name="password" className="input" placeholder="Password" />
          <div onClick={()=>setShowPass(!showPass)} className="btn  absolute -ml-14">
           {showPass ? <IoIosEye/> :<IoIosEyeOff />}
          </div>
+       </div>
+       <div >
+         <p onClick={handleForgetPass} className="hover:underline cursor-pointer">Forget Password</p>
        </div>
         <button className="btn btn-neutral mt-4">Login</button>
         <p>
